6d9571fbd0b78fa1d7af96e9a5ca7f8ca516ef9f
Tech-centre <53464793+Tech-centre@users.noreply.github.com>
2025-01-06 01:42:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,19 @@
 def is_even(number):
     return number % 2 == 0
 
+def find_square(number):
+    return number ** 2
+
 def main():
     number = int(input("Enter a number: "))
+    #To find given number is even or add
     if is_even(number):
         print(f"{number} is even.")
     else:
         print(f"{number} is odd.")
+    #To find the square of given number
+    print(f"The square of {number} is {find_square(number)}")
+    
 
 if __name__ == "__main__":
     main()
